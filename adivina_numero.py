28ff389456788e220f0a36838dab264aa61e4f47
jungarcode <jungar2806@gmail.com>
2021-12-28 08:19:12 -0500

    solucion 2 adivina un numero

--- adivina_numero.py
+++ adivina_numero.py
@@ -27,6 +27,48 @@ def adivina_numero(num):
             print("El numero es muy grande , intentalo de nuevo!!!")
             
     print(f"¡¡En hora buena!! Adivinaste el numero {num_aleatorio}")
+    
+# se hace llamada a la funcion con el numero superior a adivinar  
+    
+adivina_numero(5)
   
-# se hace llamada a la funcion con el numero superior a adivinar    
-adivina_numero(10)
+  
+# Segunda Solucion sin funcion, se establece el maximo del numero aleatorio  
+
+intentosRealizados = 0
+
+print('¡Hola! ¿Cómo te llamas?')
+miNombre = input()
+
+número = random.randint(1, 20)
+print('Bueno, ' + miNombre + ', estoy pensando en un número entre 1 y 20.')
+
+while intentosRealizados < 6:
+    print('Intenta adivinar.')
+   
+    estimación = int(input())
+    intentosRealizados = intentosRealizados + 1
+    if estimación < número:
+        print('Tu estimación es muy baja.')
+    
+
+    if estimación > número:
+
+        print('Tu estimación es muy alta.')
+
+    if estimación == número:
+
+        break
+
+if estimación == número:
+
+    intentosRealizados = str(intentosRealizados)
+
+    print('¡Buen trabajo, ' + miNombre + '! ¡Has adivinado mi número en ' +
+    intentosRealizados + ' intentos!')
+
+if estimación != número:
+
+    número = str(número)
+
+    print('Pues no. El número que estaba pensando era ' + número)
